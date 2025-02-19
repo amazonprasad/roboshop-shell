fde1dae564df2abed425e4be61584ce570da0407
muni <muni@gmail.com>
2025-02-19 21:20:22 +0530

    shellscript

---
+++ mysql.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+DATE=$(date +%F)
+USERID=$(id -u)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.... $R FAILURE $N"
+    else 
+        echo -e " $2.... $G SUCCESS $N"
+    fi 
+}
+
+if [ $? -ne 0 ]
+then 
+    echo -e " $R ERROR: You are not the root user $N"
+else 
+    echo -e " $G you are the root user  $N"
+fi
+
+
+dnf module disable mysql -y  &>> $LOGFILE
+
+VALIDATE $? " Disable mysql "
+
+cp /home/centos/roboshop-shell/msyql.repo /etc/yum.repos.d/mysql.repo
+
+VALIDATE $? " Copying files"
+
+dnf install mysql-community-server -y  &>> $LOGFILE
+
+VALIDATE $? "Install mysql"
+
+systemctl enable mysqld
+systemctl start mysqld  
+
+VALIDATE $? "enable and start mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGFILE
+
+VALIDATE $? " Set mysql password"
+
+mysql -uroot -pRoboShop@1 &>> $LOGFILE
+
+VALIDATE $? " Check password"
+
